f03e3a3cb2764ae52808e217e6570ae53ed4c942
Your Name <you@example.com>
2019-07-05 17:29:01 +0800

    Init

---
+++ mylib.cpp
@@ -0,0 +1,18 @@
+#include "hello.h"
+
+
+int hello::add(int x, int y){
+	return 0
+}
+
+int hello::minus(int x, int y){
+	return 0
+}
+
+int hello::multiply(int x, int y){
+	return 0
+}
+
+int hello::divide(int x, int y){
+	return 0
+}
